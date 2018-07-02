1ef96c8aa5aaddf39ac17a42a355b3a5346477d1
David Henley <henleydr@gmail.com>
2018-07-02 14:57:45 -0500

    Header Component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
   <div id="app">
+    <Header />
     <img src="./assets/logo.png">
+    <h1>{{ hello }}</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'app',
+  data() {
+    return { hello: 'Hello World!' };
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
+h1 {
+  color: red;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
